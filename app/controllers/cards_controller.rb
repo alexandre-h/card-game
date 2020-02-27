c58fdcafa83e20bae383182dfa2649155086a4dd
alexandre-h <herole.alexandre@gmail.com>
2020-02-27 11:27:58 +0100

    remove unwanted code

--- app/controllers/cards_controller.rb
+++ app/controllers/cards_controller.rb
@@ -1,12 +1,4 @@
 class CardsController < ApplicationController
  load_resource :card
 
-  def index
-    @cards = Card.all
-    json_response(@cards)
-  end
-
-  def show
-    json_response(@card)
-  end
 end
